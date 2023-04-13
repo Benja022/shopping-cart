a2a86505cb5f94b71aece483342d3d753024c412
Benja022 <bemanji@gmail.com>
2023-04-13 16:58:48 +0200

    Last changes

---
+++ Cheat sheet.md
@@ -0,0 +1,12 @@
+Instarlar react con typescript:
+`npx create-react-app shopping-cart --template typescript`
+Instalar MUI:
+`npm install @material-ui/core @material-ui/icons` forma.
+Instalar Query:
+`npm i react-query`
+Instalar syled components:
+`npm i styled-components @types/styled-components`
+Iniciar la aplicación:
+`npm start`
+Iniciar Typescript:
+npm
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
+// Components
+import Cart from "./Cart/Cart";
+import Item from "./Item/Item";
+import Drawer from "@mui/material/Drawer";
+import LinearProgress from "@mui/material/LinearProgress";
+import Grid from "@mui/material/Grid";
+import AddShoppingCartIcon from "@mui/icons-material//AddShoppingCart";
+import Badge from "@mui/material/Badge";
+//Styles
+import { Wrapper, StyledButton } from "./App.styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+//Funcion fetch para usar Fake Store Api
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
+const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
   );
-}
+  console.log(data);
+
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount, 0);
+
+  const handleAddToCart = (cliclkedItem: CartItemType) => {
+    setCartItems((prev) => {
+      //1. Is the item already added in the cart?
+      const isItemInCart = prev.find((item) => item.id === cliclkedItem.id);
+
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === cliclkedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      //First time the item is added
+      return [...prev, { ...cliclkedItem, amount: 1 }];
+    });
+  };
+
+  const handleRemoverFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[])
+    );
+  };
+
+  if (isLoading) return <LinearProgress />;
+
+  if (error) return <div>Something went wrong...</div>;
+
+  return (
+    <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoverFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
+};
 
 export default App;
